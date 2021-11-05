59bfd19eeb3d1f2b930e6550f13250e580dc2764
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-11-05 00:04:35 -0200

    cpffinalizado

--- validandoCPF/ValidaCPF.js
+++ validandoCPF/ValidaCPF.js
@@ -1,16 +1,57 @@
-class ValidaCPF{
-
-    constructor(cpfenviado){
-        Object.defineProperty(this, 'cpflimpo', {
-            enumerable: true,
-            get funcion(){
-                return cpfenviado.replace(/\D+/g, '');
-            }
+class Validacpf{
+    constructor(envio){
+        Object.defineProperty(this, 'limpo', {
+            value: envio.replace(/\D+/g, '')
         })
+    }
+
+eSequencia(){
+    const seq = this.limpo[0].repeat(this.limpo.length);
+    return seq === this.limpo;
+}
+
+separador(){
+    const cpfparcial = this.limpo.slice(0, -2);
+    const digito1 = this.criadigito(cpfparcial);
+    const digito2 = this.criadigito(cpfparcial + digito1);
+     this.novocpf = cpfparcial + digito1 + digito2;  
 
     }
 
+criadigito(parciall){
+   
+    let total = 0;
+    let reverso = parciall.length + 1;
+
+    for(let digito of parciall){
+        total += (reverso * Number(digito))
+        reverso--;
+
+    }
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : '0';
+
+    };
+
+
+
+valida(){
+    if( typeof this.limpo === 'undefined') return false;
+    if( this.limpo.length !== 11) return false;
+    if(this.eSequencia()) return false;
+
+    this.separador();
+  return  this.novocpf === this.limpo;
+
+   
 }
 
-const cpf = new ValidaCPF('111.111.111-11');
-console.log(cpf.cpflimpo);
+
+
+}
+
+const cpf = new Validacpf('222.222.222-20');
+
+// console.log(cpf.valida());
+
+if(!cpf.valida()){console.log('cpf Falso')}else{console.log("CPF Valido")}
